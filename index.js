74d481628c86869a52b515e471b1d3df3bc41449
JTAG <hesamgholami@yahoo.com>
2021-08-17 15:17:56 +0430

    * Add package.json and gitignore

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require ('express');
 const mongoose = require('mongoose');
-const MongoStore = require("connect-mongo")(session)
+//const MongoStore = require("connect-mongo")(session)
 const cors = require('cors');
 
 const app = express(); 
 
-mongoose.connect('mongodb://127.0.0.1:27017/online-test', {
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://online-test:secret@127.0.0.1:27017/online-test', {
             useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true
             // user:"",
             // pass:""
@@ -19,13 +19,13 @@ mongoose.set('useUnifiedTopology', true)
 app.use(express.json());
 const authRouter = require('./routes/auth');
 
-app.use(session({
-    secret: "GWRr23$@E$#2q$R#245$2",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { expires: new Date(Date.now() + 1000 * 60 * 60 * 60) },
-    store: new MongoStore({ mongooseCmoonnection: mongoose.connection })
-}))
+// app.use(session({
+//     secret: "GWRr23$@E$#2q$R#245$2",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { expires: new Date(Date.now() + 1000 * 60 * 60 * 60) },
+//     store: new MongoStore({ mongooseCmoonnection: mongoose.connection })
+// }))
 
 app.use(cors());
 app.use('/auth', authRouter);
